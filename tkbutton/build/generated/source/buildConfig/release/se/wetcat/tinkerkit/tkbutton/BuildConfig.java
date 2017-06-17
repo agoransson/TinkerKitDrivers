7a4033e7c6895b9b42e463a426f8e38485260abe
Andreas Göransson <andreasgoransson0@gmail.com>
2017-06-17 15:04:13 +0200

    Adding the missing tkbutton lib

---
+++ tkbutton/build/generated/source/buildConfig/release/se/wetcat/tinkerkit/tkbutton/BuildConfig.java
@@ -0,0 +1,16 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package se.wetcat.tinkerkit.tkbutton;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "se.wetcat.tinkerkit.tkbutton";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Fields from build type: release
+  public static final String DRIVER_NAME = "TKButton";
+  public static final int DRIVER_VERSION = 1;
+}
---
+++ tkbutton/src/main/java/se/wetcat/tinkerkit/tkbutton/TKButton.java
@@ -0,0 +1,109 @@
+package se.wetcat.tinkerkit.tkbutton;
+
+/*
+    Copyright 2017 Andreas Göransson
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+import android.util.Log;
+
+import com.google.android.things.pio.Gpio;
+import com.google.android.things.pio.GpioCallback;
+import com.google.android.things.pio.PeripheralManagerService;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * @author andreasgoransson0@gmail.com
+ */
+public class TKButton implements AutoCloseable {
+
+    private static final String TAG = TKButton.class.getSimpleName();
+
+    private final String mGpioName;
+
+    private Gpio mGpio;
+
+    private TKButtonListener mListener;
+
+    private Object mListenerLock = new Object();
+
+    protected void setListener(TKButtonListener listener) {
+        mListener = listener;
+    }
+
+    protected interface TKButtonListener {
+        void onButtonStateChanged(boolean newState);
+    }
+
+    protected TKButton(String gpioName) {
+        PeripheralManagerService peripheralManagerService = new PeripheralManagerService();
+
+        this.mGpioName = gpioName;
+
+        List<String> gpioList = peripheralManagerService.getGpioList();
+
+        if (!gpioList.contains(mGpioName)) {
+            throw new RuntimeException("The GPIO pin was not found on this hardware");
+        }
+
+        try {
+            mGpio = peripheralManagerService.openGpio(mGpioName);
+
+            mGpio.setDirection(Gpio.DIRECTION_IN);
+
+            mGpio.setEdgeTriggerType(Gpio.EDGE_FALLING);
+
+            mGpio.registerGpioCallback(mGpioCallback);
+        } catch (IOException e) {
+            Log.d(TAG, "Failed to find the GPIO, even though it should exist.", e);
+        }
+    }
+
+    private void emitValue(boolean state) {
+        synchronized (mListenerLock) {
+            mListener.onButtonStateChanged(state);
+        }
+    }
+
+    private GpioCallback mGpioCallback = new GpioCallback() {
+        @Override
+        public boolean onGpioEdge(Gpio gpio) {
+
+            try {
+                boolean state = gpio.getValue();
+                emitValue(state);
+            } catch (IOException e) {
+                Log.e(TAG, "Failed to read GPIO value", e);
+            }
+
+            return true;
+        }
+
+        @Override
+        public void onGpioError(Gpio gpio, int error) {
+            super.onGpioError(gpio, error);
+        }
+    };
+
+    @Override
+    public void close() throws Exception {
+        if (mGpio != null) {
+            mGpio.close();
+        }
+    }
+
+
+}
